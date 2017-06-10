84e0521c6091363312da3043706d506e353ea49a
David Bastien <dwbastien@gmail.com>
2017-06-09 19:21:01 -0700

    bleh

---
+++ Assets/Editor/ReorderableListDrawer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReorderableListAttribute))]
+public class ReorderableListDrawer : PropertyDrawer
+{
+    private ReorderableList list_;
+
+    public void OnEnable()
+    {
+    }
+
+    public override void OnGUI(Rect rect, SerializedProperty serializedProperty, GUIContent label)
+    {
+        SerializedProperty listProperty = serializedProperty.FindPropertyRelative("list_");
+        ReorderableList list = GetList(listProperty);
+
+        float height = 0f;
+        for (var i = 0; i < listProperty.arraySize; i++)
+        {
+            height = Mathf.Max(height, EditorGUI.GetPropertyHeight(listProperty.GetArrayElementAtIndex(i)));
+        }
+        list.elementHeight = height;
+        list.DoList(rect);
+    }
+
+    public override float GetPropertyHeight(SerializedProperty serializedProperty, GUIContent label)
+    {
+        SerializedProperty listProperty = serializedProperty.FindPropertyRelative("list_");
+        return GetList(listProperty).GetHeight();
+    }
+
+    private ReorderableList GetList(SerializedProperty serializedProperty)
+    {
+        if (list_ == null)
+        {
+            list_ = new ReorderableList(serializedProperty.serializedObject, serializedProperty);
+        }
+
+        return list_;
+    }
+}
--- Assets/Scripts/MathfExtensions.cs
+++ Assets/Scripts/MathfExtensions.cs
@@ -32,4 +32,14 @@ public static class MathfExtensions
                         RoundUpPowerOfTwo(v.y),
                         RoundUpPowerOfTwo(v.z));
     }
+
+    public static float Remap01(this float x, float minIn, float maxIn)
+    {
+        return (x - minIn) / (maxIn - minIn);
+    }
+
+    public static float Remap(this float x, float minIn, float maxIn, float minOut, float maxOut)
+    {
+        return minOut + (maxOut - minOut) * Remap01(x, minIn, maxIn);
+    }
 }
