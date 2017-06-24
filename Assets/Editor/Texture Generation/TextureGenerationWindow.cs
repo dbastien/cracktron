a031f729d7b29f644a896c2c53b0ed4a5ec579ed
David Bastien <dwbastien@gmail.com>
2017-06-23 18:12:10 -0700

    improve tex generation window and unity shader notepad++ def

--- Assets/Editor/Texture Generation/TextureGenerationWindow.cs
+++ Assets/Editor/Texture Generation/TextureGenerationWindow.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 public class TextureGenerationWindow : EditorWindow
 {
+    public int ExportWidth = 512;
+    public int ExportHeight = 512;
+
+    public Texture2D Texture;
+
     private static readonly GUILayoutOption[] GUILayoutOptionEmptyArray = new GUILayoutOption[0];
 
     [MenuItem("Window/Texture Generation")]
@@ -12,28 +15,68 @@ public class TextureGenerationWindow : EditorWindow
     {
         //get existing or spin up new window
         var window = GetWindow<TextureGenerationWindow>();
+        window.minSize = new Vector2(100, 100);
+        window.titleContent.text = "Texture Generation";
+        window.Texture = new Texture2D(512, 512, TextureFormat.ARGB32, false);
+        window.FillTexture();
+
         window.Show();
+    }
 
-        Int3 i1 = Int3.one;
-        Int3 i2 = Int3.zero;
-
-        if (i1 == i2)
+    void FillTexture()
+    {
+        for (var y = 0; y < Texture.height; ++y)
         {
+            for (var x = 0; x < Texture.width; ++x)
+            {
+                float xNormalized = x / (float)Texture.width;
+                float yNormalized = y / (float)Texture.height;
+
+                float a = 1.0f;
+                float r = Mathf.PerlinNoise(xNormalized, yNormalized);
+                float g = Mathf.PerlinNoise(xNormalized, yNormalized);
+                float b = Mathf.PerlinNoise(xNormalized, yNormalized);
+                Texture.SetPixel(x, y, new Color(r, g, b, a));
+            }
         }
+
+        Texture.Apply();
     }
 
     void OnGUI()
     {
         GUILayout.Label(" ", EditorStyles.boldLabel);
 
+        var previewLayout = new GUILayoutOption[] { GUILayout.MaxWidth(100f), GUILayout.MaxHeight(100f) };
+
         EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.IntField(Styles.resolution, 0, GUILayoutOptionEmptyArray);
+        ExportWidth = EditorGUILayout.IntField(Styles.resolutionX, ExportWidth, GUILayoutOptionEmptyArray);
+        ExportHeight = EditorGUILayout.IntField(Styles.resolutionY, ExportHeight, GUILayoutOptionEmptyArray);
         EditorGUILayout.EndHorizontal();
+
+        //https://docs.unity3d.com/ScriptReference/EditorGUI.DrawPreviewTexture.html
+        EditorGUILayout.BeginHorizontal();
+        Rect previewRect;
+        previewRect = GUILayoutUtility.GetRect(100f, 100f, previewLayout);
+        EditorGUI.DrawPreviewTexture(previewRect, Texture);
+
+        previewRect = GUILayoutUtility.GetRect(100f, 100f, previewLayout);
+        EditorGUI.DrawTextureTransparent(previewRect, Texture);
+
+        previewRect = GUILayoutUtility.GetRect(100f, 100f, previewLayout);
+        EditorGUI.DrawTextureAlpha(previewRect, Texture);
+        EditorGUILayout.EndHorizontal();        
+
+        if (GUILayout.Button("Export", GUILayoutOptionEmptyArray))
+        {
+
+        }
     }
 
     protected static class Styles
     {
         public static GUIContent resolution = new GUIContent("Resolution", "Size in pixels");
-
+        public static GUIContent resolutionX = new GUIContent("Width", "Size in pixels");
+        public static GUIContent resolutionY = new GUIContent("Height", "Size in pixels");
     }
 }
