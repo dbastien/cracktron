ce15a3cc1f857cf2c09ca0da1263892323c54726
David Bastien <dwbastien@gmail.com>
2017-12-25 18:05:10 -0800

    barrel distortion, true mask support, and better masks

--- Assets/Plugins/PostProcessingAddons/Retro.cs
+++ Assets/Plugins/PostProcessingAddons/Retro.cs
@@ -7,15 +7,27 @@ using UnityEngine.Rendering.PostProcessing;
 public sealed class Retro : PostProcessEffectSettings
 {
     [Tooltip("Number of horizontal pixels")]
-    public IntParameter ResolutionX = new IntParameter() { value = 320 };
+    public IntParameter GameResolutionX = new IntParameter() { value = 320 };
     [Tooltip("Number of vertical pixels")]
-    public IntParameter ResolutionY = new IntParameter() { value = 180 };
+    public IntParameter GameResolutionY = new IntParameter() { value = 180 };
 
+    [Tooltip("Number of horizontal pixels")]
+    public IntParameter CRTResolutionX = new IntParameter() { value = 640 };
+    [Tooltip("Number of vertical pixels")]
+    public IntParameter CRTResolutionY = new IntParameter() { value = 360 };
+    [Range(0.0f, 2.0f)]
+    public FloatParameter CRTBarrelDistortion = new FloatParameter { value = 0.3f };
+
+    [Tooltip("Pixel mask for CRT - i.e. aperture grill or shadow mask")]
     public TextureParameter CRTMask = new TextureParameter();
+    [Tooltip("How many pixels (not sub-pixels) does the mask represent?")]
+    public Vector2Parameter CRTMaskSizePixels = new Vector2Parameter { value = new Vector2(2.0f, 1.0f) };
+    public ColorParameter CRTMaskWeight = new ColorParameter() { value = Color.white };
 
     public BoolParameter ColorQuantization = new BoolParameter() { value = false  };
     [Range(1, 256)]
     public Int3Parameter ColorQuantizationBuckets = new Int3Parameter() { value = new Int3(5,5,5) };
+
     [Range(-1.0f, 1.0f)]
     public FloatParameter Brightness = new FloatParameter { value = 1.0f };
     [Range(0.0f, 2.0f)]
@@ -31,20 +43,27 @@ public sealed class RetroRenderer : PostProcessEffectRenderer<Retro>
         var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Retro"));
         var cmd = context.command;
 
-        bool hasMask = settings.CRTMask.value != null;
-        sheet.SetKeyword("_USECRTMASK_ON", hasMask);
-        if (hasMask)
-        {
-            sheet.properties.SetTexture("_CrtMask", settings.CRTMask.value);
-        }
+        int tw = settings.GameResolutionX.value;
+        int th = settings.GameResolutionY.value;
+        sheet.properties.SetVector("_GameResolution", new Vector4(tw, th, 0.0f, 0.0f));
 
         sheet.SetKeyword("_USECOLORQUANT_ON", settings.ColorQuantization.value);
         var b = settings.ColorQuantizationBuckets.value;
         sheet.properties.SetVector("_ColorQuantizationBuckets", new Vector4(b.x, b.y, b.z, 0.0f));
 
-        int tw = settings.ResolutionX.value;
-        int th = settings.ResolutionY.value;
-        sheet.properties.SetVector("_Resolution", new Vector4(tw, th, 0.0f, 0.0f));
+        tw = settings.CRTResolutionX.value;
+        th = settings.CRTResolutionY.value;
+        sheet.properties.SetVector("_CRTResolution", new Vector4(tw, th, 0.0f, 0.0f));
+        sheet.properties.SetFloat("_CRTBarrelDistortion", settings.CRTBarrelDistortion.value);
+
+        bool hasMask = settings.CRTMask.value != null;
+        sheet.SetKeyword("_USECRTMASK_ON", hasMask);
+        if (hasMask)
+        {
+            sheet.properties.SetTexture("_CRTMask", settings.CRTMask.value);
+            sheet.properties.SetVector("_CRTMaskSizePixels", settings.CRTMaskSizePixels.value);
+            sheet.properties.SetColor("_CRTMaskWeight", settings.CRTMaskWeight.value);
+        }
 
         sheet.properties.SetFloat("_Brightness", settings.Brightness);
         sheet.properties.SetFloat("_Contrast", settings.Contrast);
--- Assets/Plugins/PostProcessingAddons/Retro.shader
+++ Assets/Plugins/PostProcessingAddons/Retro.shader
@@ -10,10 +10,15 @@ Shader "Hidden/Custom/Retro"
         TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
         float4 _MainTex_TexelSize;
 
-        TEXTURE2D_SAMPLER2D(_CrtMask, sampler_CrtMask);
-        float4 _CrtMask_TexelSize;
+        float4 _GameResolution;
 
-        float4 _Resolution;
+        float4 _CRTResolution;
+        float4 _CRTMaskSizePixels;
+        TEXTURE2D_SAMPLER2D(_CRTMask, sampler_CRTMask);
+        float4 _CRTMask_TexelSize;
+        float4 _CRTMaskWeight;
+        float _CRTBarrelDistortion;
+
         float4 _ColorQuantizationBuckets;
 
         float _Brightness;
@@ -35,29 +40,55 @@ Shader "Hidden/Custom/Retro"
             float s = step(size, dist);
             return saturate(s + (1.0 - s) * brightness);
         }
+
+        float2 BarrelDistortion(float2 v, float amount) 
+        {
+            float d = dot(v, v);
+            return v * (d + amount * d * d) * amount;
+        }
         
         float4 Frag(VaryingsDefault i) : SV_Target
         {
-            float2 pr = i.texcoord * _Resolution.xy;
-            float2 puv = pr/_Resolution.xy;
-            float2 ps = i.texcoord * _MainTex_TexelSize.zw;
+            //TODO: uv space dimming
+            float2 uv = i.texcoord;
+            float2 uv_c = uv - 0.5;
 
-            float2 dist = abs(pr - (floor(pr) + 0.5));
+            uv += BarrelDistortion(uv_c, _CRTBarrelDistortion);
+            float2 uv_min = float2(0.0f, 0.0f);
+            float2 uv_max = float2(1.0f, 1.0f);
 
-            float4 col = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, floor(pr)/_Resolution.xy);
+            if ( any(step(uv_max, uv)) )
+                return float4(0.0f, 0.0f, 0.0f, 0.0f);
+            if ( any(step(uv, uv_min)) )
+                return float4(0.0f, 0.0f, 0.0f, 0.0f);
 
-            #if defined(_USECRTMASK_ON)
-                col.rgb *= SAMPLE_TEXTURE2D(_CrtMask, sampler_CrtMask, i.texcoord*_MainTex_TexelSize.zw/_CrtMask_TexelSize.zw);
-            #endif
+            //"game" rendering things
+            float2 g_pr = uv * _GameResolution.xy;
+            float2 g_puv = floor(g_pr)/_GameResolution.xy;
+            float2 g_ps = uv * _MainTex_TexelSize.zw;
+
+            float4 col = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, g_puv);
 
             #if defined(_USECOLORQUANT_ON)
                 col.rgb = round(col.rgb * (_ColorQuantizationBuckets.rgb - 1)) / (_ColorQuantizationBuckets.rgb - 1);
+            #endif
+
+            //CRT things
+            float2 c_pr = uv * _CRTResolution.xy;
+            float2 c_puv = floor(c_pr)/_CRTResolution.xy;
+
+            #if defined(_USECRTMASK_ON)
+                float2 subPixelStep = _CRTMask_TexelSize.zw / _CRTMaskSizePixels.xy;
+                float2 maskUV = (c_pr)/subPixelStep;
+                col.rgb *= saturate(SAMPLE_TEXTURE2D(_CRTMask, sampler_CRTMask, maskUV).rgb / _CRTMaskWeight.rgb);
             #endif
 
             col.rgb = Brightness(col.rgb, _Brightness);
             col.rgb = Contrast(col.rgb, _Contrast);
             col.rgb = Saturation(col.rgb, _Saturation);
 
+            //TODO: make part of crt mask?
+            float2 dist = abs(c_pr - (floor(c_pr) + 0.5));
             float sl = ScanlinePow(dist.y, 3, 0.5);
             return col * sl;
         }    
