5b6b9d33568efc6c969587215d5b5fc446ee4c47
David Bastien <dwbastien@gmail.com>
2018-05-20 19:13:25 -0700

    add orientation constraints

---
+++ Assets/Plugins/FastMath/OrientationConstraint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class OrientationConstraint
+{
+    public static Vector3 GetFacingVector(Vector3 fromPos, Vector3 toPos)
+    {
+        return (toPos - fromPos).normalized;
+    }
+
+    public static Vector3 GetFacingVectorExclusive(Vector3 fromPos, Vector3 toPos, int indexToExclude)
+    {
+        var facing = GetFacingVector(fromPos, toPos);
+        
+        return VectorSetIndex(facing, indexToExclude, 0).normalized;
+    }
+
+    public static Vector3 GetFacingVectorAroundAxis(Vector3 fromPos, Vector3 toPos, Vector3 axis)
+    {
+        var facing = GetFacingVector(fromPos, toPos);
+
+        var up = axis;
+        var right = Vector3.Cross(up, facing);
+        var forward = Vector3.Cross(right, up);
+
+        return forward.normalized;
+    }
+
+    public static Vector3 VectorSetIndex(Vector3 vec, int index, float val)
+    {
+        switch (index)
+        {
+            case 0: return new Vector3(val, vec.y, vec.z);
+            case 1: return new Vector3(vec.x, val, vec.z);
+            case 2: return new Vector3(vec.x, vec.y, val);
+        }
+
+        throw new Exception("index out of range");
+    }
+}
